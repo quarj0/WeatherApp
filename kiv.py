bc3e6e2efbe77ad5c195ef94f209d75c6e2cb38e
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-01-19 09:12:39 +0000

    update in the kiv file

--- kiv.py
+++ kiv.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 
+# Pong game made with python kivy
+# Can be played on desktop and mobile 
 
 class PongGame(Widget):
     pass
