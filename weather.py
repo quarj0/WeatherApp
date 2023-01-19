d5294bf3afdf8f6c7b8283fa4d088638b6f64f73
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-01-19 10:22:34 +0000

    update

--- weather.py
+++ weather.py
@@ -22,21 +22,28 @@ search_image = Label(image=search_bar)
 search_image.place(x=20, y=20)
 
 # text field
-textfield = Entry(root, justify="center", width=17, font=("Times New Roman", 25, "bold"), background='#404040',
-                  foreground='white')
+textfield = Entry(
+    root,
+    justify="center",
+    width=17,
+    font=("Times New Roman", 25, "bold"),
+    background="#404040",
+    foreground="white",
+)
 textfield.place(x=30, y=40)
 textfield.focus()
-box = PhotoImage(file='box.png')
+box = PhotoImage(file="box.png")
 box_image = Label(image=box)
 box_image.place(x=38, y=375)
 
 
 # defining my weather
 
+
 def getWeather():
     try:
         city = textfield.get()
-        geolocator = Nominatim(user_agent='geoapiExercises')
+        geolocator = Nominatim(user_agent="geoapiExercises")
         location = geolocator.geocode(city)
 
         tzm = TimezoneFinder()
@@ -48,14 +55,18 @@ def getWeather():
         weather_name.config(text="CURRENTLY")
 
         # Requesting for the weather with its api
-        api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=6a2cba9d48489b43b505644202086821"
+        api = (
+            "http://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&APPID=6a2cba9d48489b43b505644202086821"
+        )
         data = requests.get(api).json()
-        condition = data['weather'][0]['main']
-        description = data['weather'][0]['description']
-        temp = int(data['main']['temp'] - 273.15)
-        humidity = data['main']['humidity']
-        pressure = data['main']['pressure']
-        wind = data['main']['temp']
+        condition = data["weather"][0]["main"]
+        description = data["weather"][0]["description"]
+        temp = int(data["main"]["temp"] - 273.15)
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["main"]["temp"]
 
         t.config(text=(temp, "ºC"))
         c.config(text=condition)
@@ -64,12 +75,14 @@ def getWeather():
         h.config(text=humidity)
         d.config(text=description)
     except:
-        messagebox.showerror("Location Error!", 'cannot find location\n check spelling')
+        messagebox.showerror("Location Error!", "cannot find location\n check spelling")
 
 
 # search icon
 search_icon = PhotoImage(file="search_icon.png")
-search_icon_image = Button(image=search_icon, background='#404040', width=0, cursor='hand2', command=getWeather)
+search_icon_image = Button(
+    image=search_icon, background="#404040", width=0, cursor="hand2", command=getWeather
+)
 search_icon_image.place(x=310, y=30)
 
 # app icon
@@ -79,33 +92,39 @@ root.iconphoto(False, app_icon)
 
 
 # Logo in app, will be changed to a gif later in the upcoming versions
-logo = PhotoImage(file='logo.png')
+logo = PhotoImage(file="logo.png")
 logo_image = Label(image=logo)
 logo_image.place(x=150, y=100)
 
 # Labellings
-wd = Label(root, text='WIND', font=('Helvetica', 15, 'bold'), bg='#1ab5ef', fg='white')
+wd = Label(root, text="WIND", font=("Helvetica", 15, "bold"), bg="#1ab5ef", fg="white")
 wd.place(x=80, y=380)
 
-humid = Label(root, text='HUMIDITY', font=('Helvetica', 15, 'bold'), bg='#1ab5ef', fg='white')
+humid = Label(
+    root, text="HUMIDITY", font=("Helvetica", 15, "bold"), bg="#1ab5ef", fg="white"
+)
 humid.place(x=180, y=380)
 
-descript = Label(root, text='DESCRIPTION', font=('Helvetica', 15, 'bold'), bg='#1ab5ef', fg='white')
+descript = Label(
+    root, text="DESCRIPTION", font=("Helvetica", 15, "bold"), bg="#1ab5ef", fg="white"
+)
 descript.place(x=330, y=380)
 
-press = Label(root, text='PRESSURE', font=('Helvetica', 15, 'bold'), bg='#1ab5ef', fg='white')
+press = Label(
+    root, text="PRESSURE", font=("Helvetica", 15, "bold"), bg="#1ab5ef", fg="white"
+)
 press.place(x=520, y=380)
 
-w = Label(text="...", font=("times new roman", 18, "bold"), bg='#1ab5ef')
+w = Label(text="...", font=("times new roman", 18, "bold"), bg="#1ab5ef")
 w.place(x=75, y=410)
 
-p = Label(text="...", font=("times new roman", 18, "bold"), bg='#1ab5ef')
+p = Label(text="...", font=("times new roman", 18, "bold"), bg="#1ab5ef")
 p.place(x=530, y=410)
 
-h = Label(text="...", font=("times new roman", 18, "bold"), bg='#1ab5ef')
+h = Label(text="...", font=("times new roman", 18, "bold"), bg="#1ab5ef")
 h.place(x=200, y=410)
 
-d = Label(text="...", font=("times new roman", 18, "bold"), bg='#1ab5ef')
+d = Label(text="...", font=("times new roman", 18, "bold"), bg="#1ab5ef")
 d.place(x=330, y=410)
 
 # Time
@@ -115,7 +134,7 @@ clock = Label(root, font=("arial", 20, "bold"))
 clock.place(x=50, y=140)
 
 # Placing temperature and condition
-t = Label(font=("Arial", 30, "bold"), fg='red')
+t = Label(font=("Arial", 30, "bold"), fg="red")
 t.place(x=500, y=100)
 
 c = Label(font=("Arial", 28, "bold"))
